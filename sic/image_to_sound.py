093edac0891d72baa4ae610d1c8f2f6c20fd318e
stephenlundy-serato <stephen.lundy@serato.com>
2016-11-21 14:26:29 +1300

    Added call to load_image

--- sic/image_to_sound.py
+++ sic/image_to_sound.py
@@ -2,10 +2,15 @@ from sic import argparsing
 from sic.transformation import InverseFFT, Normalizer
 from sklearn.pipeline import Pipeline
 
+import imageprocessing
 
 def main():
     args = argparsing.parse_args()
-    print vars(args)
+
+    image_data = imageprocessing.load_image( args.image )
+
+    print image_data
+
     pipeline = Pipeline(
         [("inverse_fft", InverseFFT()),
          ("normalizer" , Normalizer())],
